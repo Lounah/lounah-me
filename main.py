2393db89f7b50867d612936e645af624f046d190
m.shchepalin <m.shchepalin@tinkoff.ru>
2022-02-13 20:00:01 +0300

    initial main page design

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request, make_response, url_for, session
+from werkzeug.utils import redirect
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='web/static')
+app.config['SESSION_TYPE'] = 'memcached'
+app.config['SECRET_KEY'] = 'some-secret-key'
 
 
 @app.route('/')
 def index():
-    return "<h1>Hello, world!</h1>"
+    theme = session.get('theme', 'theme-light')
+    return render_template("index.html", theme=theme)
+
+
+@app.route('/dark')
+def dark():
+    session['theme'] = 'theme-dark'
+    return redirect('/')
+
+
+@app.route('/light')
+def light():
+    session['theme'] = 'theme-light'
+    return redirect('/')
 
 
 if __name__ == "__main__":
